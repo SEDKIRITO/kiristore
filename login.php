bf0359ddecce9d774ed2ed3e290a6543e7257c75
Salvador <salvadored15.com@gmail.com>
2022-04-19 10:45:26 -0500

    add botonoes y edicion

--- login.php
+++ login.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-  <link rel="stylesheet" href="estilos/normalize.css">
-  <link rel="stylesheet" href="estilos/estilos.css">
-  <title>Login</title>
-</head>
-
-<body>
-  <div class="contenedor">
-  
-  <?php
+<?php
   include("cabecera.php");
-  ?>
-
-
+?>
     <div class="contenedor_menu">
 
       <h2>Iniciar sesion</h2>
--- registro.php
+++ registro.php
@@ -5,10 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilos/normalize.css">
-    <link rel="stylesheet" href="estilos/estilos.css">
     <title>Registro</title>
 </head>
 
